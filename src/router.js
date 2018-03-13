daa4bce30812a4fa610816a3872169fe094d6676
Orionlight231 <orionlight231@gmail.com>
2018-03-13 15:38:56 +0100

    first server

---
+++ src/router.js
@@ -0,0 +1,80 @@
+// var express = require('express');
+// var router = express.Router();
+// var app = express();
+//
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+//
+// module.exports = router;
+
+'use strict';
+
+const { readFile } = require('fs');
+const { parse } = require('cookie');
+const { sign, verify } = require('jsonwebtoken');
+const qs = require('querystring');
+
+const SECRET = process.env.SECRET || 'poiugyfguhijokpkoihugyfyguhijo';
+
+const userDetails = { userId: 5, role: 'admin' };
+
+const notFoundPage = '<p style="font-size: 10vh; text-align: center;">404!</p>';
+
+module.exports = (req, res) => {
+  switch (`${req.method} ${req.url}`) {
+    case 'GET /':
+      return readFile(
+        './public/index.html',
+        (err, data) => {
+          res.writeHead(
+            200,
+            {
+              'Content-Type': 'text/html',
+              'Content-Length': data.length
+            }
+          );
+          return res.end(data);
+        }
+      );
+    case 'POST /login':
+
+      var info = '';
+
+      req.on('data', function (data) {
+          info += data;
+      });
+
+      req.on('end', function () {
+          var loginData = qs.parse(info);
+          console.log(loginData);
+      });
+
+      const cookie = sign(userDetails, SECRET);
+      return readFile(
+        './public/posts.html',
+        (err, data) => {
+          res.writeHead(
+            200,
+            {
+              'Content-Type': 'text/html',
+              'Content-Length': data.length,
+              'Set-Cookie': `jwt=${cookie}; HttpOnly`
+            }
+          );
+          return res.end(data);
+        }
+      );
+
+
+    default:
+      res.writeHead(
+        404,
+        {
+          'Content-Type': 'text/html',
+          'Content-Length': notFoundPage.length
+        }
+      );
+      return res.end(notFoundPage);
+  }
+}
