ad310073c5313509cdb247cf8b7b0f2445ec6aea
Iannis Camillieri <ianniscamillieri@gmail.com>
2018-03-15 10:33:35 +0100

    added addpost page and fixed some forms issues

--- public/index.js
+++ public/index.js
@@ -1,7 +1,17 @@
+// *** VARIABLES ***
+
 var allowSubmit = false;
 var warning = [];
+var userEmail;
+var userPassword;
+
+
+// *** ACTIONS ***
 
 checkUserStatus();
+
+
+// *** FUNCTIONS ***
 
 // Global function checkUserStatus
 function checkUserStatus() {
@@ -11,10 +21,9 @@ function checkUserStatus() {
   // Function to check if token contains logged_in=true
   function redirectIfLogin(token) {
     console.log('Enter redirectIfLogin function');
-    // *** To be changed to .logged_in == true
-    if ((token) && tokenReader(token).admin == true) {
-      // *** To be changed to end point /posts
-      // window.location.href = './posts.html';
+
+    if (token && tokenReader(token).loggedin) {
+      window.location.href = '/posts.html';
       return token;
       // if no token or token not validated, listen to loggin form data
     } else {
@@ -28,23 +37,37 @@ function listenToForm() {
 
   document.getElementById("login_form").addEventListener("submit", function(event) {
     console.log('event function');
-
-    if (!allowSubmit) {
-      event.preventDefault();
-      // Get email input value
-      var userEmail = event.target.querySelectorAll("input")[0].value;
-      // Get password input value
-      var userPassword = event.target.querySelectorAll("input")[1].value;
-      // Display login warning message
-      displayLoginWarning(userEmail, userPassword);
+    event.preventDefault();
+    // Get email input value
+    userEmail = event.target.querySelectorAll("input")[0].value;
+    // Get password input value
+    userPassword = event.target.querySelectorAll("input")[1].value;
+    // Display login warning message
+    displayLoginWarning(userEmail, userPassword);
+    if (warning.length == 0) {
+      document.getElementById("login_form").submit();
     }
   });
 }
 
+// Display user warning message
+function displayLoginWarning(email, password) {
+  // Call appropriate function to build warning
+  buildLoginWarning(email, password);
+  // Set html section 'message_container' as container
+  var container = document.getElementById("message_container");
+  // Display warning in container
+  if (warning.length != 0) {
+    container.replaceChild(createList(warning, 'warning_list', 'warning'), container.firstChild);
+  } else {
+    container.innerHTML = '';
+  }
+  console.log(warning);
+  return warning;
+}
 
 // Check some input values and add infos to user global warning message
 function buildLoginWarning(email, password) {
-  console.log('Enter buildLoginWarning function');
   warning = [];
   if (email == '') {
     warning.push('Enter email');
@@ -55,22 +78,6 @@ function buildLoginWarning(email, password) {
   if (password != '' && password.length < 6) {
     warning.push('Password must be at least 6 characters');
   }
-  if (warning != []) {
-    console.log(warning);
-    return warning;
-  } else {
-    console.log(warning);
-    allowSubmit = true;
-  }
-}
-
-// Display user warning message
-function displayLoginWarning(email, password) {
-  // Call appropriate function to build warning
-  buildLoginWarning(email, password);
-  console.log(createList(warning, 'warning_list', 'warning'));
-  // Set html section 'message_container' as container
-  var container = document.getElementById("message_container");
-  // Display warning in container
-  container.replaceChild(createList(warning, 'warning_list', 'warning'), container.firstChild);
+  console.log(warning);
+  return warning;
 }
--- public/posts.js
+++ public/posts.js
@@ -1,3 +1,4 @@
+// *** VARIABLES ***
 
 var token = provideToken();
 var postsArray = providePostsList();
@@ -5,30 +6,38 @@ console.log(token, postsArray);
 
 var postsNumbers = 0;
 
-// Check token so see if user is logged in
-if ((token) && tokenReader(token).admin == true) {
-  // Print on screen "hello username !"
-  document.getElementById("user_header").innerHTML = "<p>Hello " + tokenReader(token).name + "!</p>";
-  // Print screen Add post button
-  document.getElementById("addpost_form").innerHTML =
-    '<form id="post_add" class="post_add" action="post/add" method="post">' +
-    '<input type = "submit" role = "button" name = "" value = "Add post" >' +
-    '</form>';
-  // Set visible posts to all
-  postsNumbers = postsArray.length;
-} else {
-  // Print screen 'anonymous user message'
-  document.getElementById("user_header").innerHTML =
-    '<p>As an anonymous user, you can only see the 3 latest posts. Sign in to see more posts.' +
-    '<form id="post_add" class="post_add" action="/" method="post">' +
-    '<input type="submit" role="button" name="" value="Sign in">' +
-    '</form>';
-  // Set visible posts to 3
-  postsNumbers = 3;
-}
 
-// Render posts list
+// *** ACTIONS ***
+
+checkUserStatus(token);
 renderPostsList(postsArray);
+
+
+// *** FUNCTIONS ***
+
+function checkUserStatus(token) {
+  // Check token so see if user is logged in
+  if ((token) && tokenReader(token).admin == true) {
+    // Print on screen "hello username !"
+    document.getElementById("user_header").innerHTML = "<p>Hello " + tokenReader(token).name + "!</p>";
+    // Print screen Add post button
+    document.getElementById("addpost_form").innerHTML =
+      '<form id="post_add" class="post_add" action="post/add" method="post">' +
+      '<input type = "submit" role = "button" name = "" value = "Add post" >' +
+      '</form>';
+    // Set visible posts to all
+    postsNumbers = postsArray.length;
+  } else {
+    // Print screen 'anonymous user message'
+    document.getElementById("user_header").innerHTML =
+      '<p>As an anonymous user, you can only see the 3 latest posts. Sign in to see more posts.' +
+      '<form id="post_add" class="post_add" action="/" method="post">' +
+      '<input type="submit" role="button" name="" value="Sign in">' +
+      '</form>';
+    // Set visible posts to 3
+    postsNumbers = 3;
+  }
+}
 
 function renderPostsList(postsArray) {
   // Set html section 'post_container' as container
